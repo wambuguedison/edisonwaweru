85ac0a89ccd54ae9d18d9dc33312253393ba1ad3
wambuguedison <wambuguedison@gmail.com>
2020-09-18 16:21:56 +0300

    Updates

---
+++ precache-manifest.7337b4f337822f6fa0be6dd5d2638af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c8c8ff0f2c079c68c0d024c982bd74",
+    "url": "/edisonwaweru/index.html"
+  },
+  {
+    "revision": "f1fdf51bd8679e9c2c5a",
+    "url": "/edisonwaweru/static/css/main.fc35b066.chunk.css"
+  },
+  {
+    "revision": "0625f896f0fa3e51046f",
+    "url": "/edisonwaweru/static/js/2.11581694.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/edisonwaweru/static/js/2.11581694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1fdf51bd8679e9c2c5a",
+    "url": "/edisonwaweru/static/js/main.3bba5cd4.chunk.js"
+  },
+  {
+    "revision": "fe132629d0e2d75b65e4",
+    "url": "/edisonwaweru/static/js/runtime-main.a6136804.js"
+  }
+]);
